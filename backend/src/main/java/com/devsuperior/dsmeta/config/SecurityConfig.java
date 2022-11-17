b8750faecf7c92f19f769f840f1b6ed51f41a750
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-11-16 23:26:49 -0300

    Security config

---
+++ backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dsmeta.config;
+/*
+ * Código Java para definir o acesso por outro aplicativo usando a configuração específica <cors> no framework spring...
+ * Java Code to setting access by another application using specific config <cors> at framework spring...
+ * 
+ * Commented by luisf
+ */
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.headers().frameOptions().disable();
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+
+		return http.build();
+	}
+
+	/*
+	 * my settings to use cors
+	 * commented by luisf
+	 */
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
