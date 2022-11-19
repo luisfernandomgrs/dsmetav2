0cc1435b6b8bf86c28acc377cad1668ea1bcc35c
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-11-18 23:57:07 -0300

    Heroku, bugfix at output message string - 14

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,7 +34,7 @@ public class SmsService {
 		Sale sale = saleRepository.findById(saleId).get();
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		//String amount = String.format("%.2f", sale.getAmount());
-		String amount = "1000000";
+		String amount = "10000000000";
 		String msg = "O vendedor " +sale.getSellerName() + " foi destaque em " + date
 				+ " com um total de R$ " + amount;
 
