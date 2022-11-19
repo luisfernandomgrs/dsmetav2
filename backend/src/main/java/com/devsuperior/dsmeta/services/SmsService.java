69774bdcf964d472f877698d549127f1e91edf99
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-11-18 22:58:59 -0300

    Heroku, bugfix at output message string

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -31,7 +31,7 @@ public class SmsService {
 	public void sendSms(Long saleId) {
 		Sale sale = saleRepository.findById(saleId).get();
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date; // + " com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
