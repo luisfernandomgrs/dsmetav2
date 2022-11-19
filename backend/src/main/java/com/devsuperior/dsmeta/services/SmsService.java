268c1e9d7e4a08231bd5fc553ba129aa4cf00caf
Luis Fernando Machado <luisfernandomgrs@gmail.com>
2022-11-18 23:10:16 -0300

    Heroku, bugfix at output message string - 3

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmeta.services;
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,7 +33,8 @@ public class SmsService {
 	public void sendSms(Long saleId) {
 		Sale sale = saleRepository.findById(saleId).get();
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2d", sale.getSellerName(), date, sale.getAmount());
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+			    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
